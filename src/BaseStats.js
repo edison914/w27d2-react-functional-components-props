96cce04913194282736b41d0e6e2b9bf7afa02e9
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-10-30 14:09:21 -0400

    added stats

--- src/BaseStats.js
+++ src/BaseStats.js
@@ -1,9 +1,31 @@
 import "./BaseStats.css"
 
-function BaseStats() {
+function BaseStats(props) {
+    const { hp, attack, defense, spAttack, spDef, speed } = props.stats;
+
     return (
         <div className="base-stats">
             <h1>BaseStats</h1>
+            <table>
+                <tbody>
+                    <tr>
+                    <td>Hit Points</td>
+                    <td>{hp}</td>
+                    </tr>
+                    <tr>
+                    <td>Attack</td>
+                    <td>{attack}</td>
+                    </tr>
+                    <tr>
+                    <td>Defense</td>
+                    <td>{defense}</td>
+                    </tr>
+                    <tr>
+                    <td>Speed</td>
+                    <td>{speed}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
