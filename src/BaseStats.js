b70a98697f2a16345a6e796856bed4f7ea642c69
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-10-30 13:56:09 -0400

    added basestat

---
+++ src/BaseStats.js
@@ -0,0 +1,11 @@
+import "./BaseStats.css"
+
+function BaseStats() {
+    return (
+        <div className="base-stats">
+            <h1>BaseStats</h1>
+        </div>
+    )
+}
+
+export default BaseStats;
